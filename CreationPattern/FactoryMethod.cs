2b08d5b4e9b6c60a88ad3995b17461938222a5d2
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-12-08 14:23:17 +0200

    Добавлены порождающие паттерны

---
+++ CreationPattern/FactoryMethod.cs
@@ -0,0 +1,71 @@
+﻿
+//Developer dev = new PanelDeveloper("ООО КирпичСтрой");
+//House house = dev.Create();
+
+//dev = new WoodDeveloper("Частный застройщик");
+//house = dev.Create();
+
+//Console.ReadLine();
+
+
+
+//// абстрактный класс строительной компании
+//abstract class Developer
+//{
+//    public string Name { get; set; }
+
+//    public Developer(string n)
+//    {
+//        Name = n;
+//    }
+//    // фабричный метод
+//    abstract public House Create();
+//}
+
+
+
+//// строит панельные дома
+//class PanelDeveloper : Developer
+//{
+//    public PanelDeveloper(string n) : base(n)
+//    { }
+
+//    public override House Create()
+//    {
+//        return new PanelHouse();
+//    }
+//}
+
+
+
+//// строит деревянные дома
+//class WoodDeveloper : Developer
+//{
+//    public WoodDeveloper(string n) : base(n)
+//    { }
+
+//    public override House Create()
+//    {
+//        return new WoodHouse();
+//    }
+//}
+
+//abstract class House
+//{ }
+
+//class PanelHouse : House
+//{
+//    public PanelHouse()
+//    {
+//        Console.WriteLine("Панельный дом построен");
+//    }
+//}
+//class WoodHouse : House
+//{
+//    public WoodHouse()
+//    {
+//        Console.WriteLine("Деревянный дом построен");
+//    }
+//}
+
+
