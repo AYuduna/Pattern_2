8b508293a36725ba9744b0f709b7c8e75e6247dc
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-12-11 16:48:54 +0200

    Facade correct

--- PatternStruct/Facade.cs
+++ PatternStruct/Facade.cs
@@ -1,93 +1,85 @@
-﻿//using static System.Console;
+﻿using static System.Console;
 
 
 
-//VisualStudioFacade ide = new VisualStudioFacade();
+VisualStudioFacade ide = new VisualStudioFacade();
 
-//Programmer programmer = new Programmer();
-//programmer.CreateApplication(ide);
-////programmer.Work();
+Programmer programmer = new Programmer();
+programmer.CreateApplication(ide);
+programmer.Work();
 
-//ReadKey();
-
-// //текстовый редактор
-//class TextEditor
-//{
-//    public void CreateCode()
-//    {
-//        Console.WriteLine("Написание кода");
-//    }
-//    public void Save()
-//    {
-//        Console.WriteLine("Сохранение кода");
-//    }
-//}
-//class Compiller
-//{
-//    public void Compile()
-//    {
-//        Console.WriteLine("Компиляция приложения");
-//    }
-//}
-//class CLR
-//{
-//    public void Execute()
-//    {
-//        Console.WriteLine("Выполнение приложения");
-//    }
-//    public void Finish()
-//    {
-//        Console.WriteLine("Завершение работы приложения");
-//    }
-//}
-
-//class VisualStudioFacade
-//{
-//    TextEditor textEditor = new TextEditor();
-//    Compiller compiller = new Compiller();
-//    CLR clr = new CLR();
-//    public VisualStudioFacade()
-//    {
-
-//    }
-//    public void Start()
-//    {
-//        textEditor.CreateCode();
-//        textEditor.Save();
-//        compiller.Compile();
-//        clr.Execute();
-//    }
-//    public void Stop()
-//    {
-//        clr.Finish();
-//    }
-//}
-
-//class Programmer
-//{
-//    public void CreateApplication(VisualStudioFacade facade)
-//    {
-//        facade.Start();
-//        facade.Stop();
-//    }
-//}
+ReadKey();
 
 
-////class Programmer
-////{
-////    VisualStudioFacade? _facade;
-////    public void CreateApplication(VisualStudioFacade facade)
-////    {
-////        _facade = facade;
-////    }
+class TextEditor
+{
+    public void CreateCode()
+    {
+        Console.WriteLine("Написание кода");
+    }
+    public void Save()
+    {
+        Console.WriteLine("Сохранение кода");
+    }
+}
+class Compiller
+{
+    public void Compile()
+    {
+        Console.WriteLine("Компиляция приложения");
+    }
+}
+class CLR
+{
+    public void Execute()
+    {
+        Console.WriteLine("Выполнение приложения");
+    }
+    public void Finish()
+    {
+        Console.WriteLine("Завершение работы приложения");
+    }
+}
 
-////    public void Work()
-////    {
-////        if (_facade != null)
-////        {
-////            _facade.Start();
-////            _facade.Stop();
-////        }
+class VisualStudioFacade
+{
+    TextEditor textEditor = new TextEditor();
+    Compiller compiller = new Compiller();
+    CLR clr = new CLR();
+    public VisualStudioFacade()
+    {
 
-////    }
-////}
+    }
+    public void Start()
+    {
+        textEditor.CreateCode();
+        textEditor.Save();
+        compiller.Compile();
+        clr.Execute();
+    }
+    public void Stop()
+    {
+        clr.Finish();
+    }
+}
+
+
+
+class Programmer
+{
+    VisualStudioFacade? _facade;
+    public void CreateApplication(VisualStudioFacade facade)
+    {
+        _facade = facade;
+    }
+
+    public void Work()
+    {
+        if (_facade != null)
+        {
+            _facade.Start();
+            _facade.Stop();
+        }
+
+    }
+}
